cc93882ff66f76efe5a603d5de17ed99fc34652f
hongxu.gao <hongxu.gao@ipaylinks.com>
2018-09-28 14:34:45 +0800

    commit

---
+++ spring-boot-learning-web/src/main/java/com/gaohx/web/SpringBootLearningWebApplication.java
@@ -0,0 +1,12 @@
+package com.gaohx.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLearningWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLearningWebApplication.class, args);
+	}
+}
