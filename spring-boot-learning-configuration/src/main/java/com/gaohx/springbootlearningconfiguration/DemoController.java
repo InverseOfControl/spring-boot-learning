a5cf45f4dbcf982128ffde849860860395362e84
hongxu.gao <hongxu.gao@ipaylinks.com>
2018-11-02 16:59:43 +0800

    commit

--- spring-boot-learning-configuration/src/main/java/com/gaohx/springbootlearningconfiguration/DemoController.java
+++ spring-boot-learning-configuration/src/main/java/com/gaohx/springbootlearningconfiguration/DemoController.java
@@ -3,14 +3,17 @@ package com.gaohx.springbootlearningconfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+    @Value("${aaa.security.name}")
+    private String name;
+
     @GetMapping("/demo/{str}")
     public Object demo(@PathVariable String str){
+        System.out.println(name);
         return System.getenv().get(str);
     }
 
---
+++ spring-boot-learning-configuration/src/main/java/com/gaohx/springbootlearningconfiguration/MyConfiguration.java
@@ -0,0 +1,31 @@
+package com.gaohx.springbootlearningconfiguration;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "aaa.security")
+@PropertySource("classpath:myapplication.properties")
+@Component
+public class MyConfiguration {
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
