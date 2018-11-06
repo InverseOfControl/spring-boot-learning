c43c6cfde27974e185f546660b8edab17a5e1265
hongxu.gao <hongxu.gao@ipaylinks.com>
2018-11-06 11:34:45 +0800

    commit

--- spring-cloud-learning-config-server/src/main/java/com/gaohx/springcloudlearningconfigserver/SpringCloudLearningConfigServerApplication.java
+++ spring-cloud-learning-config-server/src/main/java/com/gaohx/springcloudlearningconfigserver/SpringCloudLearningConfigServerApplication.java
@@ -1,5 +1,6 @@
 package com.gaohx.springcloudlearningconfigserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -14,23 +15,23 @@ import java.util.Set;
 @EnableEurekaClient
 public class SpringCloudLearningConfigServerApplication {
 
-	private final ContextRefresher contextRefresher;
+    private final ContextRefresher contextRefresher;
 
-	public SpringCloudLearningConfigServerApplication(ContextRefresher contextRefresher)
-	{
-		this.contextRefresher = contextRefresher;
-	}
+    public SpringCloudLearningConfigServerApplication(ContextRefresher contextRefresher) {
+        this.contextRefresher = contextRefresher;
+    }
 
-	public static void main(String[] args) {
-		System.out.println(System.getProperty("user.home"));
-		System.out.println(System.getProperty("user.dir"));
-		SpringApplication.run(SpringCloudLearningConfigServerApplication.class, args);
-	}
 
-	@GetMapping("refresh")
-	public void refresh(){
-		Set<String> set = contextRefresher.refresh();
-		System.out.println("本次更新："+set);
-	}
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("user.home"));
+        System.out.println(System.getProperty("user.dir"));
+        SpringApplication.run(SpringCloudLearningConfigServerApplication.class, args);
+    }
+
+    @GetMapping("refresh")
+    public void refresh() {
+        Set<String> set = contextRefresher.refresh();
+        System.out.println("本次更新：" + set);
+    }
 
 }
