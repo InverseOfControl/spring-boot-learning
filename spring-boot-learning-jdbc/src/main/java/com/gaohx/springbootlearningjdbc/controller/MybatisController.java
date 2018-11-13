04249a704871842fa4c1b642b71424c1a83d94f6
hongxu.gao <hongxu.gao@ipaylinks.com>
2018-11-13 15:35:01 +0800

    commit

--- spring-boot-learning-jdbc/src/main/java/com/gaohx/springbootlearningjdbc/controller/MybatisController.java
+++ spring-boot-learning-jdbc/src/main/java/com/gaohx/springbootlearningjdbc/controller/MybatisController.java
@@ -2,6 +2,8 @@ package com.gaohx.springbootlearningjdbc.controller;
 
 import com.gaohx.springbootlearningjdbc.model.City;
 import com.gaohx.springbootlearningjdbc.service.MybatisService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,11 +14,14 @@ import java.util.List;
 @RestController
 public class MybatisController {
 
+    private static final Logger logger = LoggerFactory.getLogger(MybatisController.class);
+
     @Autowired
     private MybatisService mybatisService;
 
     @GetMapping("getCity/{id}")
     public City getCity(@PathVariable String id){
+        logger.info("=========开始=========");
         return mybatisService.getCity(id);
     }
 
--- spring-boot-learning-jdbc/src/main/java/com/gaohx/springbootlearningjdbc/service/MybatisServiceImpl.java
+++ spring-boot-learning-jdbc/src/main/java/com/gaohx/springbootlearningjdbc/service/MybatisServiceImpl.java
@@ -4,6 +4,10 @@ import com.gaohx.springbootlearningjdbc.dao.CityMapper;
 import com.gaohx.springbootlearningjdbc.model.City;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -14,6 +18,9 @@ public class MybatisServiceImpl implements MybatisService {
     @Autowired(required = false)
     private CityMapper cityMapper;
 
+    @Autowired
+    private TransactionTemplate transactionTemplate;
+
     @Override
     public City getCity(String id) {
         return cityMapper.selectByPrimaryKey(BigDecimal.valueOf(Long.valueOf(id)));
@@ -21,6 +28,19 @@ public class MybatisServiceImpl implements MybatisService {
 
     @Override
     public List<City> getCityList() {
+        /*City city = new City();
+        city.setId(BigDecimal.valueOf(1113));
+        city.setName("李四");
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                try {
+                    cityMapper.insert(city);
+                } catch (Exception e) {
+                    status.setRollbackOnly();
+                }
+            }
+        });*/
         return cityMapper.getCityList("1110");
     }
 }
